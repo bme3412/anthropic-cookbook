0ed150f551a9687306cdb77fe7690c90d8020bda
Brendan Erhard <erhardbr@gmail.com>
2024-10-23 20:09:15 -0400

    adding initial claude

---
+++ 02_messages_format.py
@@ -0,0 +1,20 @@
+### BASIC SETUP
+
+from dotenv import load_dotenv
+from anthropic import Anthropic
+
+#load environment variable
+load_dotenv()
+
+#automatically looks for an "ANTHROPIC_API_KEY" environment variable
+client = Anthropic()
+
+response = client.messages.create(
+    model="claude-3-haiku-20240307",
+    max_tokens=1000,
+    messages=[
+        {"role": "user", "content": "Translate 'hello, how are you?' to French, Spanish, Italian, Portuguese. Respond with an appropriate phase for each"}
+    ]
+)
+
+print(response.content[0].text)
